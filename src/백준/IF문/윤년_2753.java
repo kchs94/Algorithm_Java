3ccba782f1f447d26af62976132586358aa78386
김충섭 <kchs94@gmail.com>
2021-06-26 16:47:39 +0900

    docs: Add 윤년_2753.java

---
+++ src/백준/IF문/윤년_2753.java
@@ -0,0 +1,18 @@
+package 백준.IF문;
+
+import java.util.Scanner;
+
+public class 윤년_2753 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int year = in.nextInt();
+
+        in.close();
+
+        if( (year%4==0 && year%100!=0) || year%400==0)
+            System.out.println(1);
+        else
+            System.out.println(0);
+    }
+}
