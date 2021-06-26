247aaaae577f9f59c818bd2ec205237bf45ae9f3
김충섭 <kchs94@gmail.com>
2021-06-26 15:52:30 +0900

    docs: Add AdivisionB_1008.java

---
+++ src/백준/입출력과사칙연산/AdivisionB_1008.java
@@ -0,0 +1,17 @@
+package 백준.입출력과사칙연산;
+
+import java.util.Scanner;
+
+public class AdivisionB_1008 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        double A = in.nextDouble();
+        double B = in.nextDouble();
+
+        System.out.println(A / B);
+
+        in.close();
+    }
+}
