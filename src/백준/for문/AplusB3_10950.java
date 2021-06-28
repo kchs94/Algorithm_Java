1f5fbb8bb4d72738673b23173da9dd2a7be56499
김충섭 <kchs94@gmail.com>
2021-06-28 15:33:34 +0900

    docs: Add AplusB3_10950.java

---
+++ src/백준/for문/AplusB3_10950.java
@@ -0,0 +1,17 @@
+package 백준.for문;
+
+import java.util.Scanner;
+
+public class AplusB3_10950 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int T = in.nextInt();   // 테스트 케이스 갯수 T 입력받기
+
+        for(int i=0; i<T; i++){ // T번 두 수의 합 입력 받고 출력하기
+            int A = in.nextInt();
+            int B = in.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
