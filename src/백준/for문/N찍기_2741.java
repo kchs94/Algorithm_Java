95241b8024ec3fa54aae0edd4b2908124c056934
김충섭 <kchs94@gmail.com>
2021-06-29 13:01:07 +0900

    docs: Update N찍기_2741.java

--- src/백준/for문/N찍기_2741.java
+++ src/백준/for문/N찍기_2741.java
@@ -1,17 +1,17 @@
 package 백준.for문;
 
-import java.util.Scanner;
-
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class N찍기_2741 {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = in.nextInt();       //자연수 N이 주어졌을 때,
+        int N = Integer.parseInt(br.readLine()); // 자연수 N이 주어졌을 때
+        br.close();
 
-        in.close();
-
-        for(int i=1; i<=N; i++)     //1부터 N까지 한 줄에 하나씩 출력
+        for(int i=1; i<=N; i++)         // 1부터 N까지 한 줄에 하나씩 출력
             System.out.println(i);
     }
 }
