e276ded4b5107f8a0631e26b534fa58a89ab9a6c
김충섭 <kchs94@gmail.com>
2021-06-30 12:02:39 +0900

    docs: Add AplusB4_10951.java

---
+++ src/백준/While문/AplusB4_10951.java
@@ -0,0 +1,20 @@
+package 백준.While문;
+
+import java.util.Scanner;
+
+public class AplusB4_10951 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int A;
+        int B;
+
+        while(in.hasNextInt()){ // hasNextInt(): 값이 있으면 true를 입력 값이 더 이상 존재하지 않으면 예외를 던진다.
+
+            A = in.nextInt();
+            B = in.nextInt();
+            System.out.println(A+B);
+        }
+        in.close();
+    }
+}
