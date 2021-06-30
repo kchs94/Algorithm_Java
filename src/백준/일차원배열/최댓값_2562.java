7ac81393faafbb07e53cb80a243e8e8b89c96650
김충섭 <kchs94@gmail.com>
2021-06-30 14:18:02 +0900

    docs: Add 최댓값_2562.java

---
+++ src/백준/일차원배열/최댓값_2562.java
@@ -0,0 +1,24 @@
+package 백준.일차원배열;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 최댓값_2562 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[9];
+        int max = 0;
+        int order = 0;
+
+        for(int i=0; i<9; i++){
+            arr[i] = in.nextInt();
+            if(arr[i] > max){
+                max = arr[i];
+                order = i+1;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(order);
+    }
+}
