8e649ceb3dfd31efcc0d0136071b0bbd317f1f62
김충섭 <kchs94@gmail.com>
2021-07-05 16:14:52 +0900

    docs: Add 시저암호 Solution.java

---
+++ src/프로그래머스/레벨1/시저암호/시저암호.java
@@ -0,0 +1,22 @@
+package 프로그래머스.레벨1.시저암호;
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        for(int i=0; i<s.length(); i++) {
+            char alpa = s.charAt(i);
+
+            if(alpa >= 'a' && alpa <= 'z') {
+                if(alpa + n > 'z') answer += (char)(alpa + n-26);
+                else answer += (char)(alpa + n);
+            }
+            else if(alpa >= 'A' && alpa <= 'Z') {
+                if(alpa + n > 'Z') answer += (char)(alpa + n-26);
+                else answer += (char)(alpa + n);
+            }
+            else answer += (char)alpa;
+
+        }
+
+        return answer;
+    }
+}
