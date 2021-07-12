d7cefd054a8673c59312f9f211ad0b70d0114958
김충섭 <kchs94@gmail.com>
2021-07-12 13:54:15 +0900

    docs: Add 정수N개의합_15596.java

---
+++ src/백준/함수/정수N개의합/정수N개의합_15596.java
@@ -0,0 +1,11 @@
+package 백준.함수.정수N개의합;
+
+public class 정수N개의합_15596 {
+    // 배열의 합을 반환하는 함수
+    public static long sum(int[] a){
+        long sum=0;  // 정수 N개의 누적 합
+        for(int i=0; i<a.length; i++)
+            sum += a[i];
+        return sum;
+    }
+}
