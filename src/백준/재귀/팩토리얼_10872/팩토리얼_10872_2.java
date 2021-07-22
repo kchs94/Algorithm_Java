8325f6e950e382429d8d7d306f19b73db64bf4ec
김충섭 <kchs94@gmail.com>
2021-07-22 11:51:13 +0900

    Add: 팩토리얼_10872.java

---
+++ src/백준/재귀/팩토리얼_10872/팩토리얼_10872_2.java
@@ -0,0 +1,22 @@
+package 백준.재귀.팩토리얼_10872;
+
+import java.util.Scanner;
+
+// 재귀함수가 아닌 반복문으로 풀기
+public class 팩토리얼_10872_2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        in.close();
+        int answer=1;
+
+        // 알고리즘
+        while(N !=0) {
+            answer = answer * N;
+            N--;
+        }
+
+        //출력
+        System.out.println(answer);
+    }
+}
