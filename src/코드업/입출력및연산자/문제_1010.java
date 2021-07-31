7bc9b20e65be786631ad0b04714e43f8a50ed476
김충섭 <kchs94@gmail.com>
2021-07-31 23:09:09 +0900

    Add 문제_1010.java

---
+++ src/코드업/입출력및연산자/문제_1010.java
@@ -0,0 +1,12 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1010 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        System.out.println(n);
+    }
+}
