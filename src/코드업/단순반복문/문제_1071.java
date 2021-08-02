913031386dcc07eb742f69913e290e6735c04cb4
김충섭 <kchs94@gmail.com>
2021-08-02 15:44:43 +0900

    Add 문제_1071.java

---
+++ src/코드업/단순반복문/문제_1071.java
@@ -0,0 +1,20 @@
+package 코드업.단순반복문;
+
+import java.util.Scanner;
+
+public class 문제_1071 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while(true){
+
+            int N = in.nextInt();
+
+            if(N == 0) break;
+
+            System.out.println(N);
+        }
+
+
+    }
+}
