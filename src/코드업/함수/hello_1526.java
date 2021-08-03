77a6fda86b3a2eff43c2a0723a01671324bbca2c
김충섭 <kchs94@gmail.com>
2021-08-03 22:27:44 +0900

    Add hello_1526.java

---
+++ src/코드업/함수/hello_1526.java
@@ -0,0 +1,11 @@
+package 코드업.함수;
+
+public class hello_1526 {
+    public static void main(String[] args) {
+        printHello();
+    }
+
+    public static void printHello(){
+        System.out.print("hello");
+    }
+}
