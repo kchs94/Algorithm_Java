6bbb1075bb3d8676faed1789f541221f4ae8f899
김충섭 <kchs94@gmail.com>
2021-08-04 13:57:13 +0900

    Add 문제_1012.java

---
+++ src/코드업/입출력및연산자/문제_1012.java
@@ -0,0 +1,13 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1012 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double n = in.nextDouble();
+        in.close();
+        System.out.format("%f", n);
+
+    }
+}
