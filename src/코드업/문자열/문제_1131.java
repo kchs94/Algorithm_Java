520158936c11f9767f5fbd3b2ea9b8aa6bfd7f4a
김충섭 <kchs94@gmail.com>
2021-08-07 23:09:53 +0900

    Add 문제_1131.java

---
+++ src/코드업/문자열/문제_1131.java
@@ -0,0 +1,12 @@
+package 코드업.문자열;
+// 한 문자 입력받기.
+import java.util.Scanner;
+
+public class 문제_1131 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().charAt(0);
+
+        System.out.println(ch);
+    }
+}
