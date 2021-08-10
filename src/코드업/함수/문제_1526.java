0464183dfab260cb1e395bb51c7156d70315773e
김충섭 <kchs94@gmail.com>
2021-08-10 13:59:15 +0900

    Add 문제_1526.java

---
+++ src/코드업/함수/문제_1526.java
@@ -0,0 +1,11 @@
+package 코드업.함수;
+
+public class 문제_1526 {
+    public static void main(String[] args) {
+        f();
+    }
+    public static void f(){
+        System.out.println("123");
+        return;
+    }
+}
