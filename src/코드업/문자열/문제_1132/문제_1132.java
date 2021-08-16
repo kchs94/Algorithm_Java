92052c7209c7665ff4d56a52faa06b1d6a6019f6
김충섭 <kchs94@gmail.com>
2021-08-16 15:21:49 +0900

    Add 문제_1095_2.java

---
+++ src/코드업/문자열/문제_1132/문제_1132.java
@@ -0,0 +1,15 @@
+package 코드업.문자열.문제_1132;
+
+import java.util.Scanner;
+
+/*
+* 1.문자열을 입력받습니다.
+* 2.문자열을 출력합니다.
+* */
+public class 문제_1132 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String word = in.next();    // 단어 입력
+        System.out.println(word);   // 단어 출력
+    }
+}
