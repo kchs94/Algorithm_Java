30f94045920e51ddd276bc2d23de083a30a08bea
김충섭 <kchs94@gmail.com>
2021-08-16 15:27:12 +0900

    Add 문제_1132_2.java

---
+++ src/코드업/문자열/문제_1132/문제_1132_2.java
@@ -0,0 +1,14 @@
+package 코드업.문자열.문제_1132;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class 문제_1132_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+
+        System.out.println(word);
+    }
+}
