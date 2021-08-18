fde935277b6ae3baca285eb344bda22b2276601c
김충섭 <kchs94@gmail.com>
2021-08-18 12:59:52 +0900

    Add 문제_1528.java

---
+++ src/백준/함수/문제_1528/문제_1528.java
@@ -0,0 +1,13 @@
+package 백준.함수.문제_1528;
+
+public class 문제_1528 {
+
+    // *을 출력하는 함수
+    private static void printStar(){
+        System.out.print('*');
+    }
+
+    public static void main(String[] args) {
+        printStar();
+    }
+}
