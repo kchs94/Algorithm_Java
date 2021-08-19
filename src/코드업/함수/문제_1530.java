afb5956f33440895a5b36f11650ae9d6172372f8
김충섭 <kchs94@gmail.com>
2021-08-19 15:31:49 +0900

    Add 문제_1530.java

---
+++ src/코드업/함수/문제_1530.java
@@ -0,0 +1,12 @@
+package 코드업.함수;
+
+public class 문제_1530 {
+
+    private static void printA(){
+        System.out.print('A');
+    }
+
+    public static void main(String[] args) {
+        printA();
+    }
+}
