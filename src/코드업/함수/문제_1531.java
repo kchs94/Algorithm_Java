0f661023be3df32f164590569ec067ef5b1c0866
김충섭 <kchs94@gmail.com>
2021-08-20 14:07:49 +0900

    Add 문제_1461.java

---
+++ src/코드업/함수/문제_1531.java
@@ -0,0 +1,11 @@
+package 코드업.함수;
+
+public class 문제_1531 {
+    public static void printOne(){
+        System.out.println(1);
+    }
+
+    public static void main(String[] args) {
+        printOne();
+    }
+}
