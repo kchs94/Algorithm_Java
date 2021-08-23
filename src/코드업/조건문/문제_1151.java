0d2729b0ad43b66eb0f199911febe03289999885
김충섭 <kchs94@gmail.com>
2021-08-23 11:38:50 +0900

    Add 문제_1151.java

---
+++ src/코드업/조건문/문제_1151.java
@@ -0,0 +1,12 @@
+package 코드업.조건문;
+
+import java.util.Scanner;
+
+public class 문제_1151 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+
+        if(number < 10) System.out.print("small");
+    }
+}
