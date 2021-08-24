a93700d4420ff87397023992233f1a586bdd0626
김충섭 <kchs94@gmail.com>
2021-08-24 12:25:44 +0900

    Add 문제_1032.java

---
+++ src/코드업/입출력및연산자/문제_1032.java
@@ -0,0 +1,12 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1032 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+
+        System.out.printf("%x", number);    // 형식화된 출력메소드
+    }
+}
