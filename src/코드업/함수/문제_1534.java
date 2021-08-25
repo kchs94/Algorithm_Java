4e6c32c64f6521d6b62a45133124561910119819
김충섭 <kchs94@gmail.com>
2021-08-25 12:13:01 +0900

    Add 문제_1534.java

---
+++ src/코드업/함수/문제_1534.java
@@ -0,0 +1,11 @@
+package 코드업.함수;
+
+public class 문제_1534 {
+    private static double f(){
+        return 3.1415926535897;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(f());
+    }
+}
