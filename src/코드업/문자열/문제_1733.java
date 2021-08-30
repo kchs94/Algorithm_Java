87798cb2a592a91d33d0cbf766d2a028f3ad6d80
김충섭 <kchs94@gmail.com>
2021-08-30 17:04:56 +0900

    Add 문제_1733.java

---
+++ src/코드업/문자열/문제_1733.java
@@ -0,0 +1,13 @@
+package 코드업.문자열;
+
+import java.util.Scanner;
+
+public class 문제_1733 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+
+        if(str.equals("IOI")) System.out.println("IOI is the International Olympiad in Informatics.");
+        else System.out.println("I don't care.");
+    }
+}
