91d2ab8df31b11b4c0aac36dec74ebe4e1ac656a
김충섭 <kchs94@gmail.com>
2021-08-30 16:38:29 +0900

    Add 문제_1036.java

---
+++ src/코드업/입출력및연산자/문제_1036.java
@@ -0,0 +1,12 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1036 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        char ch = in.next().charAt(0);
+
+        System.out.println((int)ch);
+    }
+}
