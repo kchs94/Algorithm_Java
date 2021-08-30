f1258bcf65dc7a108f56d85d923ed3c8d779a9a2
김충섭 <kchs94@gmail.com>
2021-08-30 16:58:16 +0900

    Add 문제_1416.java

---
+++ src/코드업/일차원배열/문제_1416.java
@@ -0,0 +1,12 @@
+package 코드업.일차원배열;
+
+import java.util.Scanner;
+
+public class 문제_1416 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        System.out.printf(Integer.toBinaryString(num));
+    }
+}
