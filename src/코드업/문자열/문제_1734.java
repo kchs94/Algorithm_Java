30acb35034e66fa28ff5d4f6ad697aa3bb38b466
김충섭 <kchs94@gmail.com>
2021-08-31 14:36:05 +0900

    Add 문제_1734.java

---
+++ src/코드업/문자열/문제_1734.java
@@ -0,0 +1,12 @@
+package 코드업.문자열;
+
+import java.util.Scanner;
+
+public class 문제_1734 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String id = in.next();
+
+        System.out.println("welcome! " + id);
+    }
+}
