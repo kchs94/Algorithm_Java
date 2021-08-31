cecd640d0590c77d61b17b1f10c1899b417f31fb
김충섭 <kchs94@gmail.com>
2021-08-31 13:47:23 +0900

    Add 문제_1037.java

---
+++ src/코드업/입출력및연산자/문제_1037.java
@@ -0,0 +1,12 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1037 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+
+        System.out.println((char)num);
+    }
+}
