f9810317dc1c019e2713bef74116d4c57cd15458
김충섭 <kchs94@gmail.com>
2021-09-01 15:40:03 +0900

    Add 문제_1038.java

---
+++ src/코드업/입출력및연산자/문제_1038.java
@@ -0,0 +1,13 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1038 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println( (long)a + b);
+    }
+
+}
