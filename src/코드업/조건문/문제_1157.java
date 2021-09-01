780f370750413155191b3b39eea5e1764ba4bc2b
김충섭 <kchs94@gmail.com>
2021-09-01 16:14:10 +0900

    Update 문제_1157.java

--- src/코드업/조건문/문제_1157.java
+++ src/코드업/조건문/문제_1157.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class 문제_1157 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int lenghtOfBall = in.nextInt();
+        double lenghtOfBall = in.nextDouble();
 
         if(50<=lenghtOfBall && lenghtOfBall <=60)
             System.out.println("win");
