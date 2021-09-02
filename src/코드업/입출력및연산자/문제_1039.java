348b7b906f5ce0fcdcff443439b433e3f0a33f39
김충섭 <kchs94@gmail.com>
2021-09-02 16:40:19 +0900

    Update 문제_1039.java

---
+++ src/코드업/입출력및연산자/문제_1039.java
@@ -0,0 +1,13 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1039 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long a = in.nextLong();
+        long b = in.nextLong();
+
+        System.out.println(a + b);
+    }
+}
