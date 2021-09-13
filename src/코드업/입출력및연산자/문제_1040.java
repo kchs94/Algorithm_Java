5dd9aac796694273ade96635762d90a49edd92ce
김충섭 <kchs94@gmail.com>
2021-09-13 12:23:55 +0900

    Add 문제_1040.java

---
+++ src/코드업/입출력및연산자/문제_1040.java
@@ -0,0 +1,13 @@
+package 코드업.입출력및연산자;
+
+import java.util.Scanner;
+
+public class 문제_1040 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in); // 입력준비
+
+        int num = in.nextInt(); // 입력
+
+        System.out.println(-num);   // 출력
+    }
+}
