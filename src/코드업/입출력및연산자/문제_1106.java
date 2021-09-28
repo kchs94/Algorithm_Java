6dd7a48295ae5d3b3d91af4bb4701f25d0ea5d6b
김충섭 <kchs94@gmail.com>
2021-09-28 10:00:07 +0900

    Add 문제_1106.java

---
+++ src/코드업/입출력및연산자/문제_1106.java
@@ -0,0 +1,10 @@
+package 코드업.입출력및연산자;
+/*
+* int의 최솟값과 최댓값을 출력해보자
+* 
+* */
+public class 문제_1106 {
+    public static void main(String[] args) {
+        System.out.println(Integer.MIN_VALUE + " " + Integer.MAX_VALUE);
+    }
+}
