f9b5e3a4289ec25c5e76e329989d93013454eab7
김충섭 <kchs94@gmail.com>
2021-10-21 14:54:29 +0900

    add 문제_1003.java

---
+++ src/백준/동적계획법/피보나치함수/문제_1003.java
@@ -0,0 +1,43 @@
+package 백준.동적계획법.피보나치함수;
+/*
+* 시도1: 시간초과..
+*
+* */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 문제_1003 {
+
+    static Integer[][] dp = new Integer[41][2];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while(T --> 0){
+            int N = Integer.parseInt(br.readLine());
+            fibonacci(N);
+            sb.append(dp[N][0] + " " + dp[N][1]).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    static Integer[] fibonacci(int N) {
+        // N에 대한 0, 1의 호출 횟수가 없을 때(=탐색하지 않은 값일 때)
+        if(dp[N][0] == null || dp[N][1] == null) {
+            dp[N][0] = fibonacci(N-1)[0] + fibonacci(N-2)[0];
+            dp[N][1] = fibonacci(N-1)[1] + fibonacci(N-2)[1];
+        }
+
+        return dp[N];
+    }
+}
