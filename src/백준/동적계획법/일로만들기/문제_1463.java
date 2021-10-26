a6c2f521766a36f4befb2987baa915689f7e1be2
김충섭 <kchs94@gmail.com>
2021-10-26 11:29:03 +0900

    add 문제_1463.java

--- src/백준/동적계획법/일로만들기/문제_1463.java
+++ src/백준/동적계획법/일로만들기/문제_1463.java
@@ -4,38 +4,32 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
-
 public class 문제_1463 {
 
-    static int[] arr;
     static Integer[] dp;
-
-
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        arr = new int[N+1];
-        dp = new Integer[N+1];
 
-        for(int i=1; i<=N; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        dp[0] = arr[0];
-        dp[1] = arr[1];
-
-        if(N >= 2){
-            dp[2] = arr[1] + arr[2];
-        }
+        dp = new Integer[N+1];  // 각 숫자마다 연산 횟수의 최솟값을 저장할 배열
+        dp[1] = 0;  // 숫자 1은 연산 횟수가 0니 0 대입 그리고 입력값은 1이상이므로 미리 대입해도 상관 없다.
 
         System.out.println(find(N));
     }
 
     static int find(int N) {
 
-        if(dp[N] == null) {
-            dp[N] = Math.max(find(N-2), find(N-3) + arr[N-1]) + arr[N];
+        if(dp[N] == null){
+            if(N%6 == 0){
+                dp[N] =  Math.min(find(N-1), Math.min(find(N/3), find(N/2))) + 1;
+            } else if(N%3 == 0){
+                dp[N] = Math.min(find(N/3), find(N-1)) + 1;
+            } else if(N%2 == 0){
+                dp[N] = Math.min(find(N/2), find(N-1)) + 1;
+            } else {
+                dp[N] = find(N-1) + 1;
+            }
         }
 
         return dp[N];
