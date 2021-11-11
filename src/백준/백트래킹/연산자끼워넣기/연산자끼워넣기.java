1a11fe9c3c8137ff735e953587509e84b03ea265
kchs94 <kchs94@gmail.com>
2021-11-11 13:49:14 +0900

    ADD 연산자끼워넣기.java

--- src/백준/백트래킹/연산자끼워넣기/연산자끼워넣기.java
+++ src/백준/백트래킹/연산자끼워넣기/연산자끼워넣기.java
@@ -1,39 +1,70 @@
 package 백준.백트래킹.연산자끼워넣기;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
-/*
-*
-* 1.문제 읽기 2.수도코드 작성 3.코딩 4.테스트케이스 작성
-*
-* 1. 수의 갯수 N(2<=N<=11)을 입력받는다.
-*  - int
-*
-* 2. 둘째 줄에 N개의 숫자를 입력받는다.
-*  - StringTokenizer
-*  - int형 배열
-*
-* 3. 4개의 정수(순서대로 +, -, *, / 연산자 갯수, 합은 N-1)를 공백으로 구분하여 입력받는다.
-*  - int형 배열
-*  - 길이 4, 원소의 합은 N-1
-*
-* 4. 알고리즘
-*  -
-*
-* 5. 최댓값을 출력한다.(최댓값은 10억 이하, 중간 계산되는 식도 10억 이하)
-*  - 최댓값 자료형은 int
-*
-* 6. 최솟값을 출력한다.(최솟값은 -10억 이상, 중간 계산되는 식도 -10억 이상)
-*  - 최솟값 자료형은 int
-* */
-
 public class 연산자끼워넣기 {
+
+    public static int MAX = Integer.MIN_VALUE;	// 최댓값
+    public static int MIN = Integer.MAX_VALUE;	// 최솟값
+    public static int[] operator = new int[4];	// 연산자 개수
+    public static int[] number;					// 숫자
+    public static int N;						// 숫자 개수
+
     public static void main(String[] args) throws IOException {
 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        N = Integer.parseInt(br.readLine());
+        number = new int[N];
+
+        // 숫자 입력
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 연산자 입력
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < 4; i++) {
+            operator[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dfs(number[0], 1);
+
+        System.out.println(MAX);
+        System.out.println(MIN);
 
     }
+
+    public static void dfs(int num, int idx) {
+        if (idx == N) {
+            MAX = Math.max(MAX, num);
+            MIN = Math.min(MIN, num);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) {
+            // 연산자 개수가 1개 이상인 경우
+            if (operator[i] > 0) {
+
+                // 해당 연산자를 1 감소시킨다.
+                operator[i]--;
+
+                switch (i) {
+
+                    case 0:	dfs(num + number[idx], idx + 1);	break;
+                    case 1:	dfs(num - number[idx], idx + 1);	break;
+                    case 2:	dfs(num * number[idx], idx + 1);	break;
+                    case 3:	dfs(num / number[idx], idx + 1);	break;
+
+                }
+                // 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
+                operator[i]++;
+            }
+        }
+    }
+
 }
