965f56cdaa75f3ec93e39a5970365da9e5d9675a
kchs94 <kchs94@gmail.com>
2021-12-09 20:22:34 +0900

    Add 하노이탑_11729.java

--- src/백준/백트래킹/N과M/문제_15649.java
+++ src/백준/백트래킹/N과M/문제_15649.java
@@ -7,10 +7,10 @@ import java.util.StringTokenizer;
 
 public class 문제_15649 {
 
-    public static int N;    // 정적 변수
-    public static int M;    // 정적 변수
-    public static int[] arr;
-    public static boolean[] visit;
+    public static int N;    // 1~N까지 의 수
+    public static int M;    // M개를 선택해서 나열하는 모든 경우의 수
+    public static int[] arr;    // 결과 값을 담을 배열
+    public static boolean[] visit;  // 노드의 유망성 판단.
     public static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
@@ -40,10 +40,10 @@ public class 문제_15649 {
         }
 
         for(int i=1; i<=N; i++) {
-            if(!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i;
-                dfs(depth + 1);
+            if(!visit[i]) { // i값을 사용하지 않았으면
+                visit[i] = true;    // i값을 사용한 것으로 처리
+                arr[depth] = i;     // 결과값 배열에 i값 넣기
+                dfs(depth + 1); // 다음 깊이의 노드 유망성 판단.
                 visit[i] = false;
             }
         }
--- src/백준/분할정복/이항계수3_11401.java
+++ src/백준/분할정복/이항계수3_11401.java
@@ -7,17 +7,34 @@ import java.util.StringTokenizer;
 /*
 * 순열 nPr: 서로다른 n개에서 r개를 선택하여 순서있게 일렬로 나열하는 모든 경우의 수
 * 조합 nCr: 서로다룬 n개에서 r개를 선택하는 모든 경우의 수
+* 이항계수: 1.팩토리얼 2.조합의 성질 3.동적계획법
 *
 * */
 public class 이항계수3_11401 {
-    public static void main(String[] args) {
-        System.out.println(combination(4_000_000,5));
+
+    static final int MOD = 1_000_000_007;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        dp = new int[N+1][K+1];
+
+        System.out.println(combination(N,K));
     }
 
-    public static int combination(int n, int r) {
-        if(n == r || r == 0)
-            return 1;
+    private static int combination(int N, int K) {
+
+        if(0 < dp[N][K])
+            return dp[N][K];
+
+        if(N == K || K == 0)
+            return dp[N][K] = 1;
+
         else
-            return combination(n-1, r) + combination(n-1, r-1);
+            return dp[N][K] = (combination(N-1, K)%MOD + combination(N-1, K-1)%MOD)%MOD;
     }
 }
