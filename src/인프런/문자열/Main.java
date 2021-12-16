c4fae7dd9ade987637347240eba5da13ec420015
kchs94 <kchs94@gmail.com>
2021-12-17 01:23:25 +0900

    Refactor 문자열 - 문자찾기
    1.for문을 향상된 for문으로 바꿔보기
    향상된 for문은 String으로 반복할 수 없다.
    그래서 문자열을 문자 배열로 꿔야 하는데 이때
    str.toCharArray()를 사용한다.
    
    2.문자형에서 대소문자는 Character.toLowerCase(ch)를 사용한다

--- src/인프런/문자열/Main.java
+++ src/인프런/문자열/Main.java
@@ -8,12 +8,12 @@ public class Main {
     public int solution(String str, char c) {
 
         int num=0;
-        int length = str.length();
         str = str.toLowerCase();
+        c = Character.toLowerCase(c);
 
-        for(int i=0; i<length; i++) {
-
-            if(str.charAt(i) == c)
+        // 향상된 for문으로 바꿔보기 문자열 못온다. 배열과 List 등만 가능
+        for(char x : str.toCharArray()) {   // 문자열 --> 문자 배열로 바꾸기
+            if (x == c)
                 num++;
         }
         return num;
@@ -22,8 +22,8 @@ public class Main {
     public static void main(String[] args) {
         Main T = new Main();
         Scanner in = new Scanner(System.in);
-        String str = in.next();
-        char c = in.next().toLowerCase().charAt(0);
+        String str = in.next(); // 문자열 하나 읽기
+        char c = in.next().charAt(0);
         System.out.println(T.solution(str, c));
     }
 }
