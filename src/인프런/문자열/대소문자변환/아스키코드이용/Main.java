b56ad0a3232f486ff42ee1444efccb5dd69019c0
kchs94 <kchs94@gmail.com>
2021-12-17 15:13:28 +0900

    Add 문자열 - 대소문자변환

---
+++ src/인프런/문자열/대소문자변환/아스키코드이용/Main.java
@@ -0,0 +1,40 @@
+package 인프런.문자열.대소문자변환.아스키코드이용;
+
+import java.util.Scanner;
+
+/*
+* 대소문자 변환 문제
+* 한 문자열을 입력받고 소문자는 대문자로 바꾸고 대문자는 소문자로 바꾸는 문제
+* 우선 문자열의 길이만큼 반복해서 한 문자씩 처리할건데. 문자열이 아닌 문자 단위로 처리하므로 향상된 for문과 toCharArray()로 접근한다.
+* 특정 문자가 소문자인걸 알려면 그 문자의 아스키 코드값을 알면된다. 그리고 아스키코드에서 소문자가 대문자보다 값이 작다.
+* 특정 알파벳의 대문자 소문자 값차이는 32이다.
+* 대문자에서 소문자로 만드려면 값을 -32해준다. 소문자에서 대문자를 만드려면 값을 32더해준다.
+* 그리고 구한 문자들을 이어붙이기 위해 StringBuilder를 사용한다.
+* */
+public class Main {
+
+    public String solution(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(char ch : str.toCharArray()) {
+
+            if('A' <= ch && ch <= 'Z'){                // 만약 대문자이면  // 부등호를 포함하는거 주의한다.
+                ch += 32;
+                sb.append(ch);
+            }
+            else if('a' <= ch && ch <= 'z'){           // 만약 소문자이면
+                ch -= 32;
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+
+        System.out.println(T.solution(str));
+    }
+}
