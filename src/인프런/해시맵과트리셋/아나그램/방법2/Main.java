1d1ad82231aec5fc2942c29194856837cefc9000
kchs94 <kchs94@gmail.com>
2022-01-03 13:00:41 +0900

    Add 해시맵과트리셋 - 아나그램

---
+++ src/인프런/해시맵과트리셋/아나그램/방법2/Main.java
@@ -0,0 +1,36 @@
+package 인프런.해시맵과트리셋.아나그램.방법2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/*
+* 아나그램: 어느 한 단어를 재배열해서 다른 단어와 같게 만들 수 있으면 아나그램이라 한다.
+* 아나그램 판별시 대소문자는 구분된다.
+*
+* */
+public class Main {
+    public String solution(String str1, String str2) {
+        String answer = "YES";
+        Map<Character, Integer> map = new HashMap<>();
+
+        for ( char key : str1.toCharArray()) {
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        for ( char key : str2.toCharArray()) {
+            if (!map.containsKey(key) || map.get(key) == 0)
+                return answer = "NO";
+            map.put(key, map.get(key) - 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String str1 = in.next();
+        String str2 = in.next();
+        System.out.println(T.solution(str1, str2));
+    }
+}
