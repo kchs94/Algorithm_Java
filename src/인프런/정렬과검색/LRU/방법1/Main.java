0eb398a827eb9846789f66d6927e642102d3121d
kchs94 <kchs94@gmail.com>
2022-01-10 12:57:10 +0900

    UPDATE

--- src/인프런/정렬과검색/LRU/방법1/Main.java
+++ src/인프런/정렬과검색/LRU/방법1/Main.java
@@ -16,25 +16,26 @@ import java.util.Scanner;
 public class Main {
 
     public int[] solution(int size, int numOfJobs, int[] arr) {
-        int[] cache = new int[size];
-        for (int x : arr) {
-            int pos =-1;
-            for (int i=0; i<size; i++) {
-                if (x == cache[i])  // 캐시 히트라면
+        int[] cache = new int[size];    // 캐시 선언
+        for (int x : arr) { // 작업 갯수만큼 반복
+            int pos =-1;    // 인덱스 초기화
+
+            for (int i=0; i<size; i++) {    // 캐시 히트가 있는지 없는지 확인하기 위해 반복
+                if (x == cache[i])  // 같은 값이 있으면 그 값의 인덱스 값 저장
                     pos  = i;
             }
 
-            if (pos == -1) {    // 캐시 미스라면
-                for (int i = size-1; i>=1; i--) {
+            if (pos == -1) {    // 1.캐시 미스라면
+                for (int i = size-1; i>=1; i--) {   // 값을 전체적으로 오른쪽으로 당긴다.
                     cache[i] = cache[i-1];
                 }
             }
-            else {
-                for (int i = pos; i>=1; i--) {
+            else {  // 2.캐시 히트라면
+                for (int i = pos; i>=1; i--) {  // 히트된 인덱스까지 값을 오른쪽으로 당긴다.
                     cache[i] = cache[i-1];
                 }
             }
-            cache[0] = x;
+            cache[0] = x;   // 가장 최근 값은 현재 작업으로 할당한다.
         }
         return cache;
     }
