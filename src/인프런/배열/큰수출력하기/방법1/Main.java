fb79f18e45d59629126901223c814f784d57e2b5
kchs94 <kchs94@gmail.com>
2022-03-28 23:52:01 +0900

    Update 큰수출력하기

--- src/인프런/배열/큰수출력하기/방법1/Main.java
+++ src/인프런/배열/큰수출력하기/방법1/Main.java
@@ -1,40 +1,32 @@
 package 인프런.배열.큰수출력하기.방법1;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 /*
-* 왼쪽이 앞, 오른쪽이 뒤
-* */
+ * - N을 입력받는다.
+ * - N개의 정수를 입력받는다.
+ * - 만약 N이 1이라면 1개의 정수만 출력하고 함수를 종료한다.
+ * - N-1번 반복해서 자신의 앞 수보다 크면 출력한다.
+ *
+ * - N이 1일 경우 두 수를 비교할 수 없는 예외적인 상황이 발생한다. 따라서 N이 1일 경우에는 함수를 종료한다.
+ * */
 public class Main {
+    public static void solution(int n, int[] arr) {
+        System.out.print(arr[0] + " "); // 첫 번째 수는 무조건 출력한다.
+        if (arr.length == 1) return;    // 만약 N이 1이면 비교를 할 수 없으므로 함수를 종료한다.
 
-    public String solution(int num, int[] nums) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(nums[0] + " ");
-
-        for(int i=1; i<nums.length; i++) {
-
-            if(nums[i-1] < nums[i]) {
-                sb.append(nums[i] + " ");
-            }
+        for (int i=1; i<arr.length; i++) {  // 첫 번째 원소는 출력했으니 두 번째부터 마지막 원소까지 반복한다.
+            if (arr[i] > arr[i-1])  // 자신의 수가 바로 앞수보다 크면
+                System.out.print(arr[i] + " "); // 출력한다.
         }
-
-        return sb.toString();
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] nums = new int[N];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();   // N을 입력받는다.
+        int[] arr = new int[N]; // 길이가 N인 int형 배열을 선언한다.
+        for (int i=0; i<N; i++) {
+            arr[i] = in.nextInt();
         }
-
-        Main T = new Main();
-        System.out.println(T.solution(N, nums));
+        solution(N, arr);
     }
 }
