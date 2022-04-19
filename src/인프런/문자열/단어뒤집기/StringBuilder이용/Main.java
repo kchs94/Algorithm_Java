9e561ea3d22f6dd0c7ff8ddfefe6c8beb9a4c036
kchs94 <kchs94@gmail.com>
2022-04-19 14:15:19 +0900

    Update 문자열뒤집기

--- src/인프런/문자열/단어뒤집기/StringBuilder이용/Main.java
+++ src/인프런/문자열/단어뒤집기/StringBuilder이용/Main.java
@@ -1,40 +1,30 @@
 package 인프런.문자열.단어뒤집기.StringBuilder이용;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
+    public static String[] solution(int n, String[] str) {
+        String[] reverseWords = new String[n];  // 뒤집은 문자열을 담을 배열
 
-    public String solution(String[] strings) {
-
-        /*for(int i=0; i<strings.length; i++) {
-            System.out.println(strings[i]);
-        }*/
-
-        StringBuilder result = new StringBuilder();
         StringBuilder sb;
-        for(String s : strings) {   // 단어 갯수만큼 반복
-
-            sb = new StringBuilder(s);
-            sb.reverse();
-            result.append(sb.toString()).append("\n");
+        for (int i=0; i<n; i++) {
+            sb = new StringBuilder(str[i]).reverse();   // 생성자 인자로 문자열 입력 후 뒤집기
+            reverseWords[i] = sb.toString();    // 문자열로 다시 변환
         }
-        return result.toString();
+
+        return reverseWords;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));   // 입력
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-        int N = Integer.parseInt(br.readLine());    // N 입력
-
-        String[] strings = new String[N];   // 길이가 N인 문자열 생성
-        for(int i=0; i<N; i++) {    // 단어 입력
-            strings[i] = br.readLine();
+        int n = in.nextInt();
+        String[] words = new String[n];
+        for (int i=0; i<n; i++) {
+            words[i] = in.next();
         }
-
-        Main T = new Main();
-
-        System.out.println(T.solution(strings));
+        for (String reverseWord : solution(n, words)) {
+            System.out.println(reverseWord);
+        }
     }
 }
