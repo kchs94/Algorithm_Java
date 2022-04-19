001a6e27b37255e75dc28f9d973e209629806c7f
kchs94 <kchs94@gmail.com>
2022-04-19 11:23:31 +0900

    Update 문장속단어

--- src/인프런/문자열/문장속단어/Main.java
+++ src/인프런/문자열/문장속단어/Main.java
@@ -2,27 +2,30 @@ package 인프런.문자열.문장속단어;
 
 import java.util.Scanner;
 
+/*
+ * - 공백을 기준으로 문자열을 분리합니다.
+ * - 분리된 문자열의 길이를 비교하여 비교하는 대상 문자열의 길이가 이전보다 길면 해당 문자열을 저장합니다.
+ * */
 public class Main {
-
-    public String solution(String str) {
+    public static String solution(String str) {
+        // 공백을 기준으로 문자열을 분리합니다.
         String[] words = str.split(" ");
 
-        int index = -1;
-        int max= -1;
-        for(int i=0; i< words.length; i++) {
-            if(max < words[i].length()){
-                max = words[i].length();
-                index = i;
+        // 분리된 문자열을 비교
+        String answer = "";
+        int maxWordLen = Integer.MIN_VALUE;
+        for (String word : words) {
+            if (maxWordLen < word.length()) {
+                maxWordLen = word.length();
+                answer = word;
             }
         }
-
-        return words[index];
+        return answer;
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
         Scanner in = new Scanner(System.in);
         String str = in.nextLine();
-        System.out.println(T.solution(str));
+        System.out.println(solution(str));
     }
 }
