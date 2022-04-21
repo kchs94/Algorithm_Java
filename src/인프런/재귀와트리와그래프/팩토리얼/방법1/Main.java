054a27e1c33ca840e593dea75d19498127e97c23
kchs94 <kchs94@gmail.com>
2022-04-21 14:54:54 +0900

    Update 팩토리얼

--- src/인프런/재귀와트리와그래프/팩토리얼/방법1/Main.java
+++ src/인프런/재귀와트리와그래프/팩토리얼/방법1/Main.java
@@ -3,17 +3,18 @@ package 인프런.재귀와트리와그래프.팩토리얼.방법1;
 import java.util.Scanner;
 
 public class Main {
-    public int factorial(int n) {
-        if (n == 1) // 탈출문
+    public static int recursion(int n) {
+        if (n==0) { // 0!이면 탈출
             return 1;
-        else
-            return n * factorial(n-1);  // 자기 자신 호출
+        }
+        else {
+            return n * recursion(n-1);
+        }
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        System.out.println(T.factorial(n));
+        System.out.println(recursion(n));
     }
 }
