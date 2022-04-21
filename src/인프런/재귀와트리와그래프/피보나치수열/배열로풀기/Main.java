aae8dc8fb6727613e8f6b4f234e0b41874b11693
kchs94 <kchs94@gmail.com>
2022-04-21 15:46:40 +0900

    Update 피보나치수열

--- src/인프런/재귀와트리와그래프/피보나치수열/배열로풀기/Main.java
+++ src/인프런/재귀와트리와그래프/피보나치수열/배열로풀기/Main.java
@@ -3,22 +3,24 @@ package 인프런.재귀와트리와그래프.피보나치수열.배열로풀기
 import java.util.Scanner;
 
 public class Main {
+    static int[] dp;    // 메모이제이션 역할을 할 배열 선언. 인덱스는 항 순서
 
-    static int[] dp;    // dp 배열 선언
-    public int fibo(int N) {
-        if (dp[N] > 0) return dp[N];    // 0이 아니면 미리 구한 값
-        if (N == 1) return dp[N]=1;
-        else if (N == 2) return dp[N] = 1;
-        else return dp[N] = fibo(N-2) + fibo(N-1);  // 구한 값은 배열에 저장하고 리턴한다.
+    public static int recursion(int n) {
+        if (dp[n] != 0) return dp[n];   // 앞에서 값을 구한적이 있는지 검사
+
+        if (n == 1) return dp[0] = 1;
+        else if(n == 2) return dp[1] = 1;
+        else return dp[n] = recursion(n-2) + recursion(n-1);
     }
 
     public static void main(String[] args) {
-        Main T = new Main();
         Scanner in = new Scanner(System.in);
-        int N = in.nextInt();
-        dp = new int[46];   // 인덱스는 항 번호를 의미하고 원소값은 항의 값을 의미한다.
-        for (int i=1; i<=N; i++) {
-            System.out.print(T.fibo(i) + " ");
+        int n = in.nextInt();
+        dp = new int[n+1];
+
+        for (int i=1; i<=n; i++) {
+            System.out.print(recursion(i) + " ");
         }
     }
 }
+
