33d324a97ffb2473ce8106a47530101e232b650d
kchs94 <kchs94@gmail.com>
2022-06-15 10:32:34 +0900

    Add 나머지구하기_modulo_15

---
+++ src/붓캠/데일리/modulo_15/Solution.java
@@ -0,0 +1,14 @@
+package 붓캠.데일리.modulo_15;
+import java.util.*;
+
+public class Solution {
+  public Integer modulo(int num1, int num2) {
+    if (num1 == 0) return 0;
+    if (num2 == 0) return null;
+
+    while(num1>= num2) {
+      num1 -= num2;
+    }
+    return num1;
+  }
+}
