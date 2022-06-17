044515fc54c0d0694851aa859311b281840c964e
kchs94 <kchs94@gmail.com>
2022-06-17 10:11:31 +0900

    Update

--- src/붓캠/데일리/computeSquareRoot_17/Solution.java
+++ src/붓캠/데일리/computeSquareRoot_17/Solution.java
@@ -2,18 +2,13 @@ package 붓캠.데일리.computeSquareRoot_17;
 /*
 * 제곱근을 구하는 방법 중 하나: 바빌로니아 방법
 * Xn+1 = 1/2(Xn + S*\/Xn)
-*
-*
 * */
 public class Solution {
   public static String computeSquareRoot(int num) {
-    double approximation = 1;
-    int count = 0;
+    double approximation = 1;	// 근사값
 
-    while (approximation*approximation != num) {
-      if (count == 10) break;
+    for (int i=0; i<10; i++) {	// 10번정도 반복
       approximation = (approximation + num/approximation) / 2;
-      count++;
     }
 
     return String.format("%.2f",approximation);
