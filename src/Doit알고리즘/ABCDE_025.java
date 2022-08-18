fdb7d54413ebc270d8edf634dd33ad8fb073acbb
kchs94 <kchs94@gmail.com>
2022-08-18 23:55:36 +0900

    Update

---
+++ src/Doit알고리즘/ABCDE_025.java
@@ -0,0 +1,12 @@
+package Doit알고리즘;
+/*
+* 데이터 크기: 2,000
+* 제한시간: 2초
+* 친구와 친구 관계는 마치 그래프의 정점과 간선같다는 느낌이 들었다.
+*
+* */
+public class ABCDE_025 {
+    public static void main(String[] args) {
+
+    }
+}
