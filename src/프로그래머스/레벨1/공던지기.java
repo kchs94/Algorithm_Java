b4b68e8250ee62b719cce739342c9beaf50a7301
Choongseop Kim <kchs94@gmail.com>
2022-10-23 23:39:53 +0900

    Add: 공던지기

---
+++ src/프로그래머스/레벨1/공던지기.java
@@ -0,0 +1,19 @@
+package 프로그래머스.레벨1;
+/*
+* k만큼 반복하면서 던지는 사람을 찾는 문제
+* 원형이므로 n-1 --> 1, n-->2로 던진다라는 조건을 추가한다.
+* 자꾸 인덱스와 엣지에서 헷갈린다.
+* */
+public class 공던지기 {
+    /*public int solution(int[] numbers, int k) {
+        int target = numbers[0];
+
+        for (int i=1; i<=k; i++) {
+            target = 2*i-1;
+            if (target == numbers.length-1)
+                target =
+        }
+
+        return 1;
+    }*/
+}
